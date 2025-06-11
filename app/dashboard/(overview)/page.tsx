37414b6c0b8316f59bdd69b4de0087f6d07d8277
mrauthentik <umokeuchenna2020@gmail.com>
2025-06-11 17:33:33 +0100

    Refactor RevenueChart component to fetch revenue data internally and update Page component to use Suspense for loading state

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,11 +3,13 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../../lib/data';
+import { fetchLatestInvoices, fetchCardData } from '../../lib/data';
+import { Suspense } from 'react';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { fetchRevenue } from '../../lib/data';
 
- 
 export default async function Page() {
-    const revenue = await fetchRevenue()
+    
     const latestInvoices = await fetchLatestInvoices()
     const {
         numberOfInvoices,
@@ -15,6 +17,7 @@ export default async function Page() {
         totalPaidInvoices,
         totalPendingInvoices,
     } = await fetchCardData();
+    const revenue = await fetchRevenue();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -29,10 +32,11 @@ export default async function Page() {
           value={numberOfCustomers}
           type="customers"
         /> 
-      </div>
-      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback= {<RevenueChartSkeleton />}>
+           <RevenueChart revenue={revenue} />
+        </Suspense>
+       
+       <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );
