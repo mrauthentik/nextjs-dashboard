c8d3a6ca3c6acd7195423c515f26698eefb5ed10
mrauthentik <umokeuchenna2020@gmail.com>
2025-06-11 17:23:07 +0100

    Restore Loading and Page components for dashboard functionality

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,7 +3,7 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../lib/data';
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../../lib/data';
 
  
 export default async function Page() {
